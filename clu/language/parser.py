8c7e36fbf6637099badde29e5cb4e55547d917d2
suming <suming@xiaogu-tech.com>
2017-03-11 19:33:12 +0800

    生成字典

---
+++ clu/language/parser.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import utilities
+
+DIC_EXT = u'.txt'
+
+
+def init_dic(path):
+    dic = {}
+    for root, dirs, files in os.walk(path):
+        for name in files:
+            if name.endswith(DIC_EXT):
+                file_path = os.path.join(root, name)
+                dic_type = file_path.replace(path, '', 1)
+                dic_type = dic_type.replace(DIC_EXT, '')
+                dic_type = dic_type.replace('/', '_')
+                with open(file_path, 'r') as file:
+                    for line in file:
+                        line = line.decode('utf-8').strip()
+                        if line in dic:
+                            utilities.ll(u'发现重复的字典项: {}'.format(line))
+                            dic[line].append(dic_type)
+                        else:
+                            dic[line] = [dic_type]
+    return dic
+
+
+def parse_sentencen(args):
+    utilities.is_debug = True
+    dic = init_dic(args[0])
+    utilities.ll(dic)
---
+++ main.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import argparse
+import clu.language.parser
+
+
+functions = {'simulator': clu.language.parser.parse_sentencen,
+             }
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description='clu: content and language understanding', usage='''
+        测试模拟器: python main.py simulator --parameters dict/ '西红柿得灰霉病怎么治'
+        ''', formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument('type', help='指定指令')
+    parser.add_argument('--parameters', help='指定各个参数', nargs='*')
+    args = parser.parse_args()
+    if args.type not in functions:
+        print('请输入有效的关键词类型，参见-h')
+
+    func = functions[args.type]
+    parameters = [p.decode('utf-8') for p in args.parameters]
+    func(parameters)
